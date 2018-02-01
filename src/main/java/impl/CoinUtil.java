379ae82c1dbc09fe0ae10bbf37c0e8d29b3cc555
Lom Messan Hillah <lhillah@gmail.com>
2018-02-01 17:51:38 +0100

    formatting

--- src/main/java/impl/CoinUtil.java
+++ src/main/java/impl/CoinUtil.java
@@ -5,7 +5,6 @@ import contract.Coin;
 import java.util.List;
 
 public final class CoinUtil {
-
     public static int getTotal(List<Coin> change){
         return change.stream().map(Coin::getValue).reduce(0, (a, b) -> a + b);
     }
